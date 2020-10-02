ac09c4e5667b5e4c4d3dcce947a0d2bc661cf71d
Yuri Goncharov <yurig@qualtie.com>
2020-10-02 08:05:58 -0700

    add file reference to site association
    Co-authored-by: Ben Summers <benlsummers@gmail.com>

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const fs = require('fs');
+
+var appleAppSiteAssociationContents = fs.readFileSync(path.resolve(__dirname, './.well-known/apple-app-site-association'), 'utf8');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .use(express.static(path.join(__dirname, '.well-known')))
 
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  // .get(['/.well-known/apple-app-site-association'], function (req, res) {
-  //   res.set('Content-Type', 'application/json');
-  //   res.send("{'test': '1'}");
-  // })
+  .get()
+  .get(['/.well-known/apple-app-site-association'], function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send(appleAppSiteAssociationContents);
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
